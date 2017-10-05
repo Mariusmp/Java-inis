66b8413189a1c36f22a365abc984e2e39bd73403
Mariusmp <mariusmp@gmail.com>
2017-10-05 20:22:10 +0300

    Su binariais skaičių veiksmas vienas &

--- javaOperatorsBitwise.java
+++ javaOperatorsBitwise.java
@@ -4,7 +4,7 @@ public class javaOperatorsBitwise {
 	public static void main(String[] args) {
 	int a=60;  //*60=0011 1101*/
 	int b=13;  //*13=0000 1101*/
-	int c=0;
+	int c=0;   //*va kaip įdomiai čia viskas
 	c = a & b; //*12=000 1100*/
 	System.out.println("a & b = "+c);
 	}
