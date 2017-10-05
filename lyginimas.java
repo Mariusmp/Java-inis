79d5e55a8b73f4202976c69c10b605c4e91f6a21
Mariusmp <mariusmp@gmail.com>
2017-10-05 20:23:49 +0300

    Palygina ir pasako trū ar false

--- lyginimas.java
+++ lyginimas.java
@@ -5,6 +5,7 @@ public class lyginimas {
 		System.out.println("Lyginam!");
 int a=10;
 int b=20;
+		//Pasako trū ar folsai
 System.out.println("a==b ="+(a==b));
 System.out.println("a!=b ="+(a!=b));
 System.out.println("a>b ="+(a>b));
